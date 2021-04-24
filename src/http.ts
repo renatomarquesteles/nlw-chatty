233d35ce365261780de41b8c4151d2ee3516e7a7
Renato Marques <renatomarquesteles@gmail.com>
2021-04-24 20:39:34 -0300

    Aula 04 - Landing

---
+++ src/http.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+import path from 'path';
+
+import './database';
+
+import { routes } from './routes';
+
+const app = express();
+
+// Configuração para utilizar o html
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set('views', path.join(__dirname, '..', 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/pages/client', (request, response) => {
+  return response.render('html/client.html');
+});
+
+const http = createServer(app); // Cria protocolo http
+const io = new Server(http); // Cria protocolo ws
+
+app.use(express.json());
+
+app.use(routes);
+
+export { http, io };
